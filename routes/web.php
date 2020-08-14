9a9ed2e611420715c7c4e9c0a019d8df99105194
soy-software <soysoftware@gmail.com>
2020-08-14 08:40:28 -0500

    envio de datos en web

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    // Artisan::call('cache:clear');
+    /// Artisan::call('cache:clear');
     // Artisan::call('config:clear');
     // Artisan::call('config:cache');
     // Artisan::call('storage:link');
